6bc418962e195530f2a45dda9d0f1561a8b2643a
Rick Copeland <rick@synapp.io>
2014-09-04 12:35:26 -0400

    Even more typos and weird stuff
    Signed-off-by: Rick Copeland <rick@synapp.io>

--- chapman/model/m_task.py
+++ chapman/model/m_task.py
@@ -68,6 +68,7 @@ class TaskStateResource(Resource):
         if not ts:
             log.error('Trying to acquire tsr %s that is already acquired',
                 self.id)
+            return False
         if msg_id == ts.mq[0]:
             return True
         return False
